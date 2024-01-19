2dd2de76ab4a45748aeb5d156db6f4053a3cb7ea
Raffaele Della Vecchia <dellavecchiar@gmail.com>
2024-01-19 15:09:49 +0100

    second commit of proloco website

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     
     return (
         <>
-            <BrowserRouter>
+            <BrowserRouter basename="/ProLocoNuscoWebSite">
                 <Routes>
                     <Route path='/' element={
                         <div style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover' }}>
